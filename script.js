20d8f06be52da6ede5e8724a537d7fc61b50ceb2
felipscarvalho <felipecarvalho5520@gmail.com>
2024-04-24 21:37:38 -0300

    ✨ Filter function added

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 import emojisData from "./emoji.json" assert { type: "json" }
+import gitmojiEmojis from "./gitmoji-emojis.json" assert { type: "json" }
+import { writeFile } from "fs"
 
 const emojis = emojisData.emojis
 
-console.log(emojis[0])
+function filterEmojis(emojis, gitmojiEmojis) {
+	let filteredEmojis = []
+
+	for (let i = 0; i < emojis.length; i++) {
+		for (let x = 0; x < gitmojiEmojis.length; x++) {
+			if (emojis[i]["shortname"] === gitmojiEmojis[x]["shortname"]) {
+				filteredEmojis.push(emojis[i])
+			}
+		}
+	}
+
+	writeFile("filtered-emojis.json", JSON.stringify(filteredEmojis), () => {})
+}
+
+filterEmojis(emojis, gitmojiEmojis)
